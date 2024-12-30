29e286e093b187ab9049dfd1c8e4b9e155ab9524
dpngqd@umsystem.edu <dat.peter.nguyen1@gmail.com>
2024-12-30 09:03:51 -0600

    Program now generates an 8x8 Chessboard using pygame module

--- Chessboard.py
+++ Chessboard.py
@@ -1,3 +1,35 @@
+import pygame
+import ChessPieces
+
 class Chessboard:
-    def __init__(self):
-        pass
+    pass
+
+def generate_board():  # Generates the chessboard and the pieces in starting position.
+
+    # Variables used in chessboard generation
+    CHESS_ROWS = 8
+    xCoordinate = 0
+    yCoordinate = 0
+
+    pygame.init()  # Initiate pygame function/methods.
+
+    # Pieces
+    screen = pygame.display.set_mode((1000, 800))
+    darkSquare = pygame.image.load("images/ChessDarkSquare.png").convert()
+    lightSquare = pygame.image.load("images/ChessLightSquare.png").convert()
+    whiteRook = pygame.image.load("images/ChessRookWhite.png").convert()
+    blackRook = pygame.image.load("images/ChessRookBlack.png").convert()
+
+    for index in range(CHESS_ROWS):
+        for i in range(8):
+            if index % 2 == 0:
+                screen.blit(lightSquare, (xCoordinate, yCoordinate))
+            else:
+                screen.blit(darkSquare, (xCoordinate, yCoordinate))
+            index += 1
+            xCoordinate += 64
+
+        xCoordinate = 0
+        yCoordinate += 64
+
+    pygame.display.flip()
--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 import Chessboard
+import ChessPieces
+import pygame
 
-print("test")
+running = True
+
+pygame.display.set_caption('Better Chess Analysis')
+
+
+Chessboard.generate_board()
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
