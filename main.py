7d5566c653dc82513c878e5ce18bcf7a89eaa586
dpngqd@umsystem.edu <dat.peter.nguyen1@gmail.com>
2025-02-06 23:29:20 -0600

    main.py updated to accomadate for if user is importing games or starting new games.

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
+import os
 import pygame
 import Chessboard
 
 pygame.display.set_caption('Better Chess Analysis')
 
-Chessboard.generate_board()
+condition = True
+
+while condition:
+    user_input = input("Are you importing a game? (y/n) ").lower()
+    if user_input == 'y':
+        file_name = input("Enter the game file name (without .txt): ") + ".txt"
+        if os.path.isfile(file_name):
+            # Read moves & notes from file
+            moves, notes = Chessboard.import_game_from_text(file_name)
+            # Now pass these moves to the updated generate_board function
+            Chessboard.generate_board(imported_game=(moves, notes))
+            condition = False
+        else:
+            print("The file doesn't exist! Please try again.")
+    elif user_input == 'n':
+        # Start a fresh game
+        Chessboard.generate_board()
+        condition = False
+    else:
+        print("Please enter y or n.")
